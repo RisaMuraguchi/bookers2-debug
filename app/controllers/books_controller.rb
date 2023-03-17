32a2e95ec89ffe900547b328118dd695bba0010a
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-03-17 07:45:54 +0000

    [Finish]bookers2-debug

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,6 +6,7 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def create
@@ -21,6 +22,11 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    if  @book.user == current_user
+       redirect_to books_path
+    else
+       render "edit"
+    end
   end
 
   def update
@@ -28,19 +34,19 @@ class BooksController < ApplicationController
     if @book.update(book_params)
       redirect_to book_path(@book), notice: "You have updated book successfully."
     else
-      render "edit"
+      render :edit
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
 end
