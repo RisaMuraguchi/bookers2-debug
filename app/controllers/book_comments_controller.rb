c2a62744d813c8e9a0156ff503efcb34b6070377
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-04-05 11:28:04 +0000

    訂正

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -9,6 +9,8 @@ class BookCommentsController < ApplicationController
 
   def destroy
     BookComment.find(params[:id]).destroy
+    @book = Book.find(params[:book_id])
+    render :index
   end
 
 
