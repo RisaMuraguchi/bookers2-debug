e6ba024197177f0201f745d1f9886159fa3fca6d
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-04-08 10:47:47 +0000

    View追加

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,4 +5,13 @@ class Group < ApplicationRecord
   validates :name, presence: true
   validates :introduction, presence: true
   has_one_attached :image
+
+  def get_image(width, height)
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpg')
+    end
+    image.variant(resize_to_limit: [width, height]).processed
+  end
+
 end
