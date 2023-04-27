1a3d6b7b46738d34eb9857a390d7e14a857cf533
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-04-27 12:46:06 +0000

    チャット機能

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,9 @@ Rails.application.routes.draw do
     resource :group_users, only: [:create, :destroy]
   end
 
-  resources :rooms, only: [:create, :show, :index]
+  resources :messages, only: [:create]
+  resources :rooms, only: [:create,:show]
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
